14ac5058352f28bfd5568260723eda80b039705a
yuanjingsong <532473857@qq.com>
2019-12-28 23:19:43 +0800

    fix segment recipe is null

--- src/index/lipa_cache.c
+++ src/index/lipa_cache.c
@@ -19,14 +19,27 @@ struct LIPA_cacheItem* new_lipa_cache_item(struct ctxtTableItem* ctxtTableItem, 
     new_cacheItem ->tableItemPtr = ctxtTableItem;
 
     new_cacheItem ->kvpairs = g_hash_table_new_full(g_feature_hash, g_feature_equal, NULL, NULL);
+    if (sr == NULL) {
 
+        GSequenceIter* chunkIter = g_sequence_get_begin_iter(ctxtTableItem->segment_ptr->chunks);
+        GSequenceIter* chunkEnd = g_sequence_get_end_iter(ctxtTableItem->segment_ptr->chunks);
 
-    GHashTableIter tableIter;
-    gpointer key, value;
-    g_hash_table_iter_init(&tableIter, sr->kvpairs);
-    while (g_hash_table_iter_next(&tableIter, &key, &value)) {
-        g_hash_table_insert(new_cacheItem->kvpairs, &key, value);
+        for(; chunkIter != chunkEnd; chunkIter = g_sequence_iter_next(chunkIter)){
+            struct chunk* c = g_sequence_get(chunkIter);
+            g_hash_table_insert(new_cacheItem->kvpairs, &c->fp, TEMPORARY_ID):
+        }
+
+    }else {
+        GHashTableIter tableIter;
+        gpointer key, value;
+        g_hash_table_iter_init(&tableIter, sr->kvpairs);
+        while (g_hash_table_iter_next(&tableIter, &key, &value)) {
+            g_hash_table_insert(new_cacheItem->kvpairs, &key, value);
+        }
+
     }
+
+
 
     return new_cacheItem;
 }
